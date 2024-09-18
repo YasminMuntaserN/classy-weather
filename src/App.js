db6ee76991b7f8281c22610dadd95c216b1879eb
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-18 10:31:22 +0300

    feat: add new states

--- src/App.js
+++ src/App.js
@@ -38,9 +38,14 @@ class App extends React.Component {
     super(props);
     this.state = {location:"london" ,
       isLoading:false,
+      displayLocation:"",
+      weather:{}
     };
+
     this.fetchWeather =this.fetchWeather.bind(this);
   }
+
+
   async fetchWeather(){
     try {
       this.setState({isLoading:true});
@@ -56,14 +61,15 @@ class App extends React.Component {
   
       const { latitude, longitude, timezone, name, country_code } =
         geoData.results.at(0);
-      console.log(`${name} ${convertToFlag(country_code)}`);
+      this.setState({displayLocation :`${name} 
+                    ${convertToFlag(country_code)}`});
   
       // 2) Getting actual weather
       const weatherRes = await fetch(
-        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
+        `https://api.open-meteo.com/v1/forecast? latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
       );
       const weatherData = await weatherRes.json();
-      console.log(weatherData.daily);
+      this.setState({weather :weatherData.daily});
     } catch (err) {
       console.err(err);
     }finally{

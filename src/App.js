c2854a71243e223bee39ede05e3a7b6b9566d896
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-18 10:26:01 +0300

    feat: add isLoading state

--- src/App.js
+++ src/App.js
@@ -36,11 +36,14 @@ function formatDay(dateStr) {
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {location:"london"};
+    this.state = {location:"london" ,
+      isLoading:false,
+    };
     this.fetchWeather =this.fetchWeather.bind(this);
   }
   async fetchWeather(){
     try {
+      this.setState({isLoading:true});
       // 1) Getting location (geocoding)
       const geoRes = await fetch(
         `https://geocoding-api.open-meteo.com/v1/search? 
@@ -63,6 +66,8 @@ class App extends React.Component {
       console.log(weatherData.daily);
     } catch (err) {
       console.err(err);
+    }finally{
+      this.setState({isLoading:false});
     }
   }
     render(){
@@ -77,6 +82,9 @@ class App extends React.Component {
                         e.target.value})}/>
           </div>
           <button>Get Weather</button>
+          {
+            this.state.isLoading && <p>Loading ...</p>
+          }
         </div>
       );
     }
